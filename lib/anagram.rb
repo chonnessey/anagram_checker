72546bfbe71f2ee115e23e4d1f326f7a54dba76c
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-16 16:23:50 -0700

    add passing test that ignores whitespace and punctuation

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,12 +6,13 @@ class Anagram
 
   def anagram_checker
     anagram_str = ""
-    word1_array = @word1.split('')
-    word2_array = @word2.split('')
-      if word1_array.sort == word2_array.sort
+    # word1_array = @word1.split('')
+    # word2_array = @word2.split('')
+      if @word1.gsub(/\W/, "").split('').sort == @word2.gsub(/\W/, "").split('').sort
         anagram_str = "These words are anagrams!"
-      elsif @word1.(/\W/) == @word2.(/\W/)
-        anagram_str = "These words are anagrams!"
+      # elsif @word1.gsub(/\W/, "") == @word2.gsub(/\W/, "")
+
+      #   anagram_str = "These words are anagrams!"
       end
     return anagram_str
   end
@@ -22,8 +23,9 @@ class Anagram
     vowel_str = ""
     if (vowel_check_one === nil) or (vowel_check_two === nil)
       vowel_str = "You need to input real words!"
+    else
+      anagram_checker()
     end
+    return vowel_str
   end
 end
-
-# (@word1 =~ /[aeiouy]/i) && (@word2 =~ /[aeiouy]/i)
