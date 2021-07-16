077e745be7d10f80c1db62d7fa1129f2bfc394fb
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-16 15:21:29 -0700

    WIP: still hung up on vowel logic

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,19 +6,14 @@ class Anagram
 
   def anagram_checker
     vowels = ['a', 'e', 'i', 'o', 'u']
-    i = 0
     anagram_str = ""
     word1_array = @word1.split('')
     word2_array = @word2.split('')
-    until i > vowels.length
-      i += 1
-      puts i
       if word1_array.sort == word2_array.sort
         anagram_str = "These words are anagrams!"
-      elsif word1_array !=~ (/aeiou/)
+      elsif word1_array.include?(vowels.join)
         anagram_str = "You need to input a real word!"
       end
-    end
     return anagram_str
   end
 end
