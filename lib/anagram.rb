4cc490632c486d6504698c612b6f868b659c5bee
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-16 10:44:02 -0700

    add second passing test to be case insensitive

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,13 @@
 class Anagram
   def initialize(word1, word2)
-    @word1 = word1.split('')
-    @word2 = word2.split('')
+    @word1 = word1.downcase
+    @word2 = word2.downcase
   end
 
   def anagram_checker
     anagram_str = ""
-    if @word1.sort.downcase == @word2.sort.downcase
+
+    if @word1.split('').sort == @word2.split('').sort
       anagram_str = "These words are anagrams!"
     end
     return anagram_str
