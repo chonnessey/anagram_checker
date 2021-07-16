caa50f22fda7a3ebe9d74feea04aad17d3b41230
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-16 10:37:15 -0700

    add first passing test to check if two words are anagrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,14 @@
 class Anagram
-  def initialize(word)
-    @word = word.split('')
+  def initialize(word1, word2)
+    @word1 = word1.split('')
+    @word2 = word2.split('')
   end
 
   def anagram_checker
-    temp_array = []
-    @word.each do |word1|
-      
+    anagram_str = ""
+    if @word1.sort == @word2.sort
+      anagram_str = "These words are anagrams!"
+    end
+    return anagram_str
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,6 +4,7 @@ require 'anagram'
 
 describe('Anagram#anagram_checker') do
   it('will check if a word is an anagram') do
-    
+    anagram = Anagram.new("act", "cat")
+    expect(anagram.anagram_checker).to(eq("These words are anagrams!"))
   end
 end
