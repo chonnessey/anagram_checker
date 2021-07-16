4970e246f564e753bc398420bd7deac01b30ed34
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-16 09:42:30 -0700

    WIP: working on logic

---
+++ lib/anagram.rb
@@ -0,0 +1,11 @@
+class Anagram
+  def initialize(word)
+    @word = word.split('')
+  end
+
+  def anagram_checker
+    temp_array = []
+    @word.each do |word1|
+      
+  end
+end
---
+++ spec/anagram_spec.rb
@@ -0,0 +1,9 @@
+require 'pry'
+require 'rspec'
+require 'anagram'
+
+describe('Anagram#anagram_checker') do
+  it('will check if a word is an anagram') do
+    
+  end
+end
