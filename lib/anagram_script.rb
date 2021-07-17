98088ac8370d785e85510bcdeb55fe9cc7787832
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-16 20:33:20 -0700

    add script file to program

---
+++ lib/anagram_script.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+
+puts "Enter two words or phrases to see if they are either an anagram or an antigram!"
+puts "Word(s) or Phrase One:"
+input1 = gets.chomp
+puts "Word(s) or Phrase Two:"
+input2 = gets.chomp
+
+ana_v_anti = Anagram.new("#{input1}", "#{input2}")
+
+ava_v_anti.anagram_checker()
+ava_v_anti.antigram_checker()
+ava_v_anti.vowel_checker()
+
