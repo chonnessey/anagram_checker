f4c45ea0b8ba83572585a242587313d25805fb80
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-25 20:32:44 -0700

    refactor logic into one method and update tests

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,45 +6,47 @@ class Anagram
 
   def anagram_checker
     anagram_str = ""
-      if @word1.split('').sort == @word2.split('').sort
+      if !/[aeiou]/.match?(@word1.split('').sort.to_s) || !/[aeiou]/.match?(@word2.split('').sort.to_s)
+        anagram_str = "You need to input real words!"
+      elsif @word1.split('').sort == @word2.split('').sort
         anagram_str = "These words are anagrams!"
-      elsif vowel_checker() == "You need to input real words!"
-        anagram_str = "These phrases are anagrams.. but you need to input real words!"
+      # elsif vowel_checker() == "You need to input real words!"
+      #   anagram_str = "These phrases are anagrams.. but you need to input real words!"
       end
     return anagram_str
   end
 
-  def vowel_checker
-    vowel_check_one = @word1.match(/aeiou/)
-    vowel_check_two = @word2.match(/aeiou/)
-    vowel_str = ""
-    if (vowel_check_one === nil) or (vowel_check_two === nil)
-      vowel_str = "You need to input real words!"
-    else
-      anagram_checker()
-      antigram_checker()
-    end
-    return vowel_str
-  end
+  # def vowel_checker
+  #   vowel_check_one = @word1.match?(/aeiou/)
+  #   vowel_check_two = @word2.match?(/aeiou/)
+  #   vowel_str = ""
+  #   if (vowel_check_one === nil) or (vowel_check_two === nil)
+  #     vowel_str = "You need to input real words!"
+  #   else
+  #     anagram_checker()
+  #     antigram_checker()
+  #   end
+  #   return vowel_str
+  # end
 
-  def antigram_checker
-    antigram1_array = []
-    antigram2_array = []
-    word1_array = @word1.split('').sort
-    word2_array = @word2.split('').sort
-    antigram_str = ""
-    word1_array.each do |letter|
-      antigram1_array << letter.scan(/[a-zA-Z]/).to_s
-      word2_array.each do |element|
-        antigram1_array << element.scan(/[a-zA-Z]/).to_s
-        if antigram1_array != antigram2_array
-          antigram_str = "These words are an antigram!"
-        else
-          anagram_checker()
-          vowel_checker()
-        end
-      end
-    end
-    return antigram_str
-  end
+  # def antigram_checker
+  #   antigram1_array = []
+  #   antigram2_array = []
+  #   word1_array = @word1.split('').sort
+  #   word2_array = @word2.split('').sort
+  #   antigram_str = ""
+  #   word1_array.each do |letter|
+  #     antigram1_array << letter.scan(/[a-zA-Z]/).to_s
+  #     word2_array.each do |element|
+  #       antigram1_array << element.scan(/[a-zA-Z]/).to_s
+  #       if antigram1_array != antigram2_array
+  #         antigram_str = "These words are an antigram!"
+  #       else
+  #         anagram_checker()
+  #         vowel_checker()
+  #       end
+  #     end
+  #   end
+  #   return antigram_str
+  # end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,34 +3,40 @@ require 'rspec'
 require 'anagram'
 
 describe('Anagram#anagram_checker') do
+  it('will check if two words are valid by checking for vowels') do
+    anagram = Anagram.new("BcD", "cBD")
+    expect(anagram.anagram_checker).to(eq("You need to input real words!"))
+  end
+
   it('will check if two words are an anagram') do
     anagram = Anagram.new("act", "cat")
     expect(anagram.anagram_checker).to(eq("These words are anagrams!"))
   end
 
   it('will check if two words are an anagram regardless of case') do
-    anagram = Anagram.new("AcT", "cAt")
+    anagram = Anagram.new("CaT", "AcT")
     expect(anagram.anagram_checker).to(eq("These words are anagrams!"))
   end
 
-  it('will check if two words are an anagram regardless of case and filter out words with no vowels') do
-    anagram = Anagram.new("BcD", "cBd")
-    anagram.vowel_checker()
-    expect(anagram.anagram_checker).to(eq("These phrases are anagrams.. but you need to input real words!"))
-  end
 
-  it('will check if two words are valid by checking for vowels') do
-    anagram = Anagram.new("pkmn", "nmkp")
-    expect(anagram.vowel_checker).to(eq("You need to input real words!"))
-  end
+  # it('will check if two words are an anagram regardless of case and filter out words with no vowels') do
+  #   anagram = Anagram.new("BcD", "cBd")
+  #   anagram.vowel_checker()
+  #   expect(anagram.anagram_checker).to(eq("These phrases are anagrams.. but you need to input real words!"))
+  # end
 
-  it('will check if two words are anagrams regardless of spaces and punctuation') do
-    anagram = Anagram.new("The Morse Code", "Here come dots!")
-    expect(anagram.anagram_checker).to(eq("These words are anagrams!"))
-  end
+  # it('will check if two words are valid by checking for vowels') do
+  #   anagram = Anagram.new("pkmn", "nmkp")
+  #   expect(anagram.vowel_checker).to(eq("You need to input real words!"))
+  # end
 
-  it('will check if two words are antigrams regardless of spaces and punctuation') do
-    anagram = Anagram.new("Hey hEy", "cooL CoOl")
-    expect(anagram.antigram_checker).to(eq("These words are an antigram!"))
-  end
+  # it('will check if two words are anagrams regardless of spaces and punctuation') do
+  #   anagram = Anagram.new("The Morse Code", "Here come dots!")
+  #   expect(anagram.anagram_checker).to(eq("These words are anagrams!"))
+  # end
+
+  # it('will check if two words are antigrams regardless of spaces and punctuation') do
+  #   anagram = Anagram.new("Hey hEy", "cooL CoOl")
+  #   expect(anagram.antigram_checker).to(eq("These words are an antigram!"))
+  # end
 end
